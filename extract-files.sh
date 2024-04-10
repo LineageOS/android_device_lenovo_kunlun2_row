2925994ef796304e67d30a9cd934833868e3bb39
npjohnson <johnsonnolen@gmail.com>
2024-04-10 23:25:03 +0200

    kunlun2_row: Align extractors with templates
    * These now directly support VENDOR_COMMON!
    
    Change-Id: I6c4e2226fea90be3dcc29dccbd7e23d24b2473f5

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2019 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -17,5 +18,6 @@ set -e
 export DEVICE=kunlun2_row
 export DEVICE_COMMON=sdm710-common
 export VENDOR=lenovo
+export VENDOR_COMMON=lenovo
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
